056403e24f8e6122117d2f521fdfd54ba8b7d65e
Vidwa De Seram <vidwadeseram2002@gmail.com>
2023-12-07 03:15:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 
 console.log("Attempting MongoDB connection...");
-
 mongoose
   .connect(process.env.MY_SECRET, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 5000, // Adjust the timeout as needed
   })
-  .then(() => console.log("MongoDB Connected from environment secret"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+  .then(() => {
+    console.log("MongoDB Connected");
+    process.exit(0); // Exit successfully
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1); // Exit with error
+  });
